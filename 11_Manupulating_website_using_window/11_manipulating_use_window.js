0ee631266add49ad5ed6a7ce304afb934aaaaa11
SanjayPaul4u <98152041+SanjayPaul4u@users.noreply.github.com>
2022-05-12 12:49:00 +0530

    this is my first commit on this file
    no  optional commit

---
+++ 11_Manupulating_website_using_window/11_manipulating_use_window.js
@@ -0,0 +1,28 @@
+console.log("Manipulating websites usig  Window. ");
+
+let a = window;
+// alert("Don't click!")
+// console.log(a);
+// let p = prompt("It will be destroy your computer! if you not Enter your name.")
+// console.log(p);
+
+// let con = confirm("Do you really want to delete this page?????")
+// console.log(con);
+
+
+
+a =innerHeight;
+a =innerWidth;
+a = scrollX;
+a= scrollY;
+
+a = location;
+a = location.reload;
+a = location.href;
+a = location.toString();
+
+a = window.history;
+a = history.go;
+a = history.go(1);
+a = history.go(-1);
+console.log(a);
